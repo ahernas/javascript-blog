9bae26ef0913251ba9097cefe109234799387eec
Anna Garczarek <ania.garczarek@gmail.com>
2020-02-19 00:01:59 +0100

    Correct the project code

--- js/script.js
+++ js/script.js
@@ -42,13 +42,6 @@ const titleClickHandler = function(event) {
     targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
-console.log(links);
-
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
-}
-
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
