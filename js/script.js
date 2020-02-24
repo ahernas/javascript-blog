92dcaed899b44192f4e91e50adaadf502ba3d884
Anna Garczarek <ania.garczarek@gmail.com>
2020-02-25 00:43:14 +0100

    Generate numbers of tag

--- js/script.js
+++ js/script.js
@@ -96,7 +96,7 @@ generateTitleLinks();
 
 function generateTags() {
 
-  let allTags = [];
+  let allTags = {};
 
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
@@ -126,10 +126,12 @@ function generateTags() {
       html =  html + linkHTML + ' ';
      // console.log(html);
 
-     /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-        /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+      /* [NEW] add tag to allTags object */
+         allTags[tag] = 1;
+       } else {
+        allTags[tag]++;
       }
     /* END LOOP: for each tag */
      }
@@ -141,8 +143,18 @@ function generateTags() {
     /* [NEW] find list of tags in right column */
   const tagList = document.querySelector('.tags');
 
-  /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+  for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+  }
+  /* [NEW] END LOOP: for each tag in allTags: */
+
+  /*[NEW] add HTML from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
 }
 
 generateTags();
